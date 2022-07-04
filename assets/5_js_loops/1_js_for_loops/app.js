51fa53e5e1b9751541455d1ed5f142c1b2f60325
Andrew Zdunek <andrewzdunek.dev@gmail.com>
2022-07-04 20:50:09 +0100

    adding js_loops

---
+++ assets/5_js_loops/1_js_for_loops/app.js
@@ -0,0 +1,34 @@
+/*
+
+for (
+    [initialExpression];
+    [condition];
+    [incrementExpression]
+)
+
+*/
+
+//     1            2     3
+for (let i = 1; i <= 100; i++){
+    console.log(i);
+}
+
+// Another examples is printing out strings
+
+for (let i = 1; i <= 6; i++) {
+    console.log("Da ba dee da ba daa");
+}
+
+// Even numbers
+
+for(let i = 2; i <= 20; i+=2){
+    console.log(i)
+}
+
+// Odd Numbers
+
+for(let i = 1; i <= 20; i+=2) {
+    console.log(i)
+}
+
+
